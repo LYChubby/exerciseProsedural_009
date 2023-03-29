5a2fd0447f39b70b90b7752e75b9d9ddedd890ad
LYChubby <f4uzi345@gmail.com>
2023-03-29 13:47:26 +0700

    Membuat Status Kelulusan

--- exerciseProsedural_009/exerciseProsedural_009.cpp
+++ exerciseProsedural_009/exerciseProsedural_009.cpp
@@ -5,3 +5,12 @@ float rerata(float m, float b) {
 	return (m + b) / 2;
 }
 
+string hasil(float r, float m) {
+	if (r >= 70 || m > 80) {
+		return "Diterima";
+	}
+	else {
+		return "Ditolak";
+	}
+}
+
